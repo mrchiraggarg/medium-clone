26e9361a59d31ea5dc9b4b2add7eafc752bde1a1
mrchiraggarg <gargchirag728@gmail.com>
2025-07-02 14:51:41 +0530

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $categories = [
+            'Technology',
+            'Health',
+            'Lifestyle',
+            'Education',
+            'Travel',
+            'Food',
+            'Finance',
+            'Entertainment',
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($categories as $category)
+            \App\Models\Category::create(['name' => $category]);
+
+        Post::factory(100)->create();
     }
 }

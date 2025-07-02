f8f2d8504d20fefe76eb1d9282ceb52337fb2177
mrchiraggarg <gargchirag728@gmail.com>
2025-07-02 14:38:34 +0530

    commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
 }

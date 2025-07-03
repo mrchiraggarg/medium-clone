44383a7870eb0fe6fb56919d09f26c72f6453709
mrchiraggarg <gargchirag728@gmail.com>
2025-07-03 23:26:14 +0530

    commit

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-8">
-                <form action="{{ route('post.store') }}" method="POST">
+                <form action="{{ route('post.store') }}" enctype="multipart/form-data" method="POST">
                     @csrf
 
                     <!-- Image -->

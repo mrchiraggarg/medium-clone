2120a07f10eec042f31ce0a4862abfeb880d0e39
mrchiraggarg <gargchirag728@gmail.com>
2025-07-03 22:57:05 +0530

    commit

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                Create A New Post
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-8">
+                <form action=""></form>
             </div>
         </div>
     </div>

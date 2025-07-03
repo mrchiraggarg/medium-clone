7db1558a31551322cd595445dd5ce9fc2962a837
mrchiraggarg <gargchirag728@gmail.com>
2025-07-03 22:05:41 +0530

    commit

--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -2,9 +2,12 @@
     <li class="me-2">
         <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active" aria-current="page">All</a>
     </li>
-    @foreach ($categories as $category)
+    @forelse ($categories as $category)
     <li class="me-2">
         <a href="#" class="inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">{{$category->name}}</a>
     </li>
-    @endforeach
+    @empty
+    <li class="me-2">
+        <span class="inline-block px-4 py-2 text-gray-500 dark:text-gray-400">No category available</span>
+    </li>
 </ul>

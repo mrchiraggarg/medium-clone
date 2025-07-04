e1febf2794fb1c675efab98bb0b20e324462c03a
mrchiraggarg <gargchirag728@gmail.com>
2025-07-04 16:15:17 +0530

    commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,9 +11,12 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'image',
         'title',
+        'slug',
         'content',
         'category_id',
+        'published_at',
     ];
 }

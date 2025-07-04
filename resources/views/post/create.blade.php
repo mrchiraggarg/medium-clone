abced56bda2ba4f8aea33a698f390dede138ca22
mrchiraggarg <gargchirag728@gmail.com>
2025-07-04 16:00:09 +0530

    commit

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -9,14 +9,14 @@
                     <div class="mb-4">
                         <x-input-label for="title" :value="__('Title')" />
                         <x-text-input id="title" class="block mt-1 w-full" type="text" name="title"
-                            :value="old('title')" required autofocus />
+                            :value="old('title')" autofocus />
                         <x-input-error :messages="$errors->get('title')" class="mt-2" />
                     </div>
 
                     <!-- Category Id -->
                     <div class="mb-4">
                         <x-input-label for="category_id" :value="__('Category')" />
-                        <select id="category_id" class="block mt-1 w-full" name="category_id" required>
+                        <select id="category_id" class="block mt-1 w-full" name="category_id">
                             <option value="">Select Category</option>
                             @foreach ($categories as $category)
                                 <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -29,7 +29,9 @@
                     <div class="mb-4">
                         <x-input-label for="content" :value="__('Content')" />
                         <x-input-textarea id="content" class="block mt-1 w-full" type="text" name="content"
-                            :value="old('content')" required autofocus />
+                            autofocus>
+                            {{ old('content') }}
+                        </x-input-textarea>
                         <x-input-error :messages="$errors->get('content')" class="mt-2" />
                     </div>
 
@@ -37,7 +39,7 @@
                     <div class="mb-4">
                         <x-input-label for="image" :value="__('Image')" />
                         <x-text-input id="image" class="block mt-1 w-full" type="file" name="image"
-                            :value="old('image')" required autofocus />
+                            :value="old('image')" autofocus />
                         <x-input-error :messages="$errors->get('image')" class="mt-2" />
                     </div>
 

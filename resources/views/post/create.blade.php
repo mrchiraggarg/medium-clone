a147a28129153151b45fa2f637b23e61f91d4c23
mrchiraggarg <gargchirag728@gmail.com>
2025-07-04 15:49:46 +0530

    commit

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,14 +5,6 @@
                 <form action="{{ route('post.store') }}" enctype="multipart/form-data" method="POST">
                     @csrf
 
-                    <!-- Image -->
-                    <div class="mb-4">
-                        <x-input-label for="image" :value="__('Image')" />
-                        <x-text-input id="image" class="block mt-1 w-full" type="file" name="image"
-                            :value="old('image')" required autofocus />
-                        <x-input-error :messages="$errors->get('image')" class="mt-2" />
-                    </div>
-
                     <!-- Title -->
                     <div class="mb-4">
                         <x-input-label for="title" :value="__('Title')" />
@@ -21,12 +13,32 @@
                         <x-input-error :messages="$errors->get('title')" class="mt-2" />
                     </div>
 
+                    <!-- Category Id -->
+                    <div class="mb-4">
+                        <x-input-label for="category_id" :value="__('Category')" />
+                        <select id="category_id" class="block mt-1 w-full" name="category_id" required>
+                            <option value="">Select Category</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                        <x-input-error :messages="$errors->get('category_id')" class="mt-2" />
+                    </div>
+
                     <!-- Content -->
                     <div class="mb-4">
                         <x-input-label for="content" :value="__('Content')" />
                         <x-input-textarea id="content" class="block mt-1 w-full" type="text" name="content"
                             :value="old('content')" required autofocus />
                         <x-input-error :messages="$errors->get('content')" class="mt-2" />
+                    </div>
+
+                    <!-- Image -->
+                    <div class="mb-4">
+                        <x-input-label for="image" :value="__('Image')" />
+                        <x-text-input id="image" class="block mt-1 w-full" type="file" name="image"
+                            :value="old('image')" required autofocus />
+                        <x-input-error :messages="$errors->get('image')" class="mt-2" />
                     </div>
 
                     <x-primary-button type="submit">

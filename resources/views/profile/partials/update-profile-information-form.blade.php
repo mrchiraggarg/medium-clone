dfd91307a054125723532ffd64469c990e0c13c7
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 13:14:29 +0530

    commit

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -21,7 +21,7 @@
         <div class="mb-4">
             @if ($user->image)
                 <div>
-                    <img src="{{ Storage::url($user->image) }}" class="rounded-full h-20 w-20">
+                    <img src="{{ Storage::url($user->image) }}" class="rounded-full object-cover h-20 w-20">
                 </div>
             @endif
 

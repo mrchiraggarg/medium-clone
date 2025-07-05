2a3a868222fb7ad8d4bcb377c525c7888942268e
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 17:50:31 +0530

    commit

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -3,7 +3,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="flex">
-                    <div class="flex-1"></div>
+                    <div class="flex-1">
+                        <h1 class="text-5xl text-white">{{ $user->name }}</h1>
+                    </div>
                     <div class="w-[320px] border-l px-8">
                         <x-user-avatar :user="$user" size="w-24 h-24" />
                         <h3 class="text-white">{{ $user->name }}</h3>

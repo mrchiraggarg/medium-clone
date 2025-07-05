82b0a404b482d4fe6464afb56a5bdff68bbcc3c2
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 13:14:08 +0530

    commit

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,7 +2,10 @@
     <div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-8">
-                POST
+                <h1 class="text-5xl mb-4">{{ $post->title }}</h1>
+                <div class="flex gap-4">
+                    <img src="{{ Storage::url($post->user->image) }}" class="rounded-full w-20 h-20 object-cover" alt="{{ $post->user->name }}">
+                </div>
             </div>
         </div>
     </div>

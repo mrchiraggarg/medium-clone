be9f0312bced088e417cc9169c1decf49cb01a99
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 13:11:10 +0530

    commit

---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-8">
+                POST
+            </div>
+        </div>
+    </div>
+</x-app-layout>

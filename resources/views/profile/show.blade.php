783b2fbee9a65db06b2da12c330f919470ad0150
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 17:22:06 +0530

    commit

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -3,9 +3,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="flex">
-                    <div></div>
-                    <div class="w-[320px] border-l">
+                    <div class="flex-1"></div>
+                    <div class="w-[320px] border-l px-8">
                         <x-user-avatar :user="$user" size="w-24 h-24" />
+                        <h3>{{ $user->name }}</h3>
+                        <p class="text-gray-500">26K Followers</p>
+                        <p>{{ $user->bio }}</p>
                     </div>
                 </div>
             </div>

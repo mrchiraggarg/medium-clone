5fdaf301fe398b5fc9f25ebec56ae5104ca97789
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 21:00:38 +0530

    commit

--- database/migrations/2025_07_05_152652_create_followers_table.php
+++ database/migrations/2025_07_05_152652_create_followers_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('followers', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps('created_at')->nullable();
         });
     }
 

69a137c5bd9e29c792ff84630ffbb62536479117
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 22:23:42 +0530

    commit

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -20,12 +20,16 @@
                         <h3 class="text-white">{{ $user->name }}</h3>
                         <p class="text-gray-500">{{ $user->followers()->count() ?? 0 }} Followers</p>
                         <p class="text-white">{{ $user->bio }}</p>
-                        @if (auth()->user() && auth()->user()->id !== $user->id)
-                            <div>
-                                <button class="bg-emerald-600 rounded-full px-2 py-2 text-white mt-4"
-                                    x-text="following ? 'Unfollow : 'Follow'"></button>
+                        @if (auth()->check() && auth()->id() !== $user->id)
+                            <div x-data="{ following: {{ $user->isFollowedBy(auth()->user()) ? 'true' : 'false' }} }">
+                                <button
+                                    class="rounded-full px-4 py-2 text-white mt-4 transition duration-200 ease-in-out"
+                                    :class="following ? 'bg-red-600 hover:bg-red-700' : 'bg-emerald-600 hover:bg-emerald-700'"
+                                    x-text="following ? 'Unfollow' : 'Follow'">
+                                </button>
                             </div>
                         @endif
+
                     </div>
                 </div>
             </div>

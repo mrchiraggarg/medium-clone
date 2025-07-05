b1f5c84c8410bb9f47d5347d47a26703f7f9761b
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 17:14:58 +0530

    commit

--- resources/views/components/user-avatar.blade.php
+++ resources/views/components/user-avatar.blade.php
@@ -1,7 +1,7 @@
-@props(['user'])
+@props(['user', 'size' => 'w-12 h-12'])
 
 @if ($user->image)
-    <img src="{{ $user->imageUrl() }}" class="rounded-full w-12 h-12 object-cover" alt="{{ $user->name }}">
+    <img src="{{ $user->imageUrl() }}" class="rounded-full {{ $size }} object-cover" alt="{{ $user->name }}">
 @else
-    <img src="https://i.pravatar.cc/300" class="rounded-full w-12 h-12 object-cover">
+    <img src="https://i.pravatar.cc/300" class="rounded-full {{ $size }} object-cover">
 @endif

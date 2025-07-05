a42f13745c78b5caced1b5b606fcaca5721cc903
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 17:07:41 +0530

    commit

--- resources/views/components/user-avatar.blade.php
+++ resources/views/components/user-avatar.blade.php
@@ -3,5 +3,5 @@
 @if ($user->image)
     <img src="{{ $user->imageUrl() }}" class="rounded-full w-12 h-12 object-cover" alt="{{ $user->name }}">
 @else
-    <img src="https://i.pravatar.cc/300" class="rounded-full w-12 h-12 object-cover" alt="{{ $user->name }}">
+    <img src="https://i.pravatar.cc/300" class="rounded-full w-12 h-12 object-cover">
 @endif

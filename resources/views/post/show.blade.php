93caac95ba9ab5e0c87ea53ff0dc9ebd4a5abc54
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 16:06:16 +0530

    commit

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -25,12 +25,16 @@
                     </div>
                 </div>
 
-                <div class="mt-8 border-t border-b">
-                    <div>
-                        <img src="{{ $post->imageUrl() }}" alt="{{ $post->title }}" class="w-full">
-                        <div class="mt-4">
-                            {{ $post->content }}
-                        </div>
+                <div class="mt-8 p-4 border-t border-b">
+                    <button class="flex gap-2 text-gray-500 hover:text-gray-900">
+                        30.4K Claps
+                    </button>
+                </div>
+
+                <div class="mt-8">
+                    <img src="{{ $post->imageUrl() }}" alt="{{ $post->title }}" class="w-full">
+                    <div class="mt-4">
+                        {{ $post->content }}
                     </div>
                 </div>
             </div>

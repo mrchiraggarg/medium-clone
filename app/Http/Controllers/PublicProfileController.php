b74dffd8f4ceddbe080a0968f6c1e69318d587ce
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 16:31:00 +0530

    commit

--- app/Http/Controllers/PublicProfileController.php
+++ app/Http/Controllers/PublicProfileController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PublicProfileController extends Controller
 {
-    public function show(Request $request, string $username)
+    public function show(Request $request, User $user)
     {
-        
+
     }
 }

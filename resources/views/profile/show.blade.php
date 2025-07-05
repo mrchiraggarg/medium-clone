7f2370019c785c5f047d967c2ad90d6236e79d6b
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 17:00:05 +0530

    commit

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,6 +2,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="flex">
+                    <div></div>
+                    <div class="w-[320px] border-l"></div>
+                </div>
             </div>
         </div>
     </div>

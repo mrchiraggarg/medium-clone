5c3e2d2154f3b88f8ce1681090b26155960b283e
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 17:07:23 +0530

    commit

--- resources/views/components/user-avatar.blade.php
+++ resources/views/components/user-avatar.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-</div>
+@props(['user'])
+
+@if ($user->image)
+    <img src="{{ $user->imageUrl() }}" class="rounded-full w-12 h-12 object-cover" alt="{{ $user->name }}">
+@else
+    <img src="https://i.pravatar.cc/300" class="rounded-full w-12 h-12 object-cover" alt="{{ $user->name }}">
+@endif

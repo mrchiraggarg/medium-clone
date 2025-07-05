5122a65d572f10d37bb02f7485cbfcd812865a7f
mrchiraggarg <gargchirag728@gmail.com>
2025-07-05 23:27:53 +0530

    commit

--- resources/views/components/follow-ctr.blade.php
+++ resources/views/components/follow-ctr.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+@props(['users'])
+
+<div x-data="{
+    following: {{ $user->isFollowedBy(auth()->user()) ? 'true' : 'false' }},
+    followersCount: {{ $user->followers()->count() }},
+    follow() {
+        this.following = !this.following
+        axios.post('/follow/{{ $user->id }}')
+            .then(res => {
+                this.followersCount = res.data.followersCount
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+}" class="w-[320px] border-l px-8">
+    {{ $slot }}
 </div>

e426a7772f631baed673c0dfe66c617a309b5f99
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 11:30:35 +0530

    commit

--- app/Models/Clap.php
+++ app/Models/Clap.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Clap extends Model
 {
-    //
+    protected $fillable = ['post_id', 'user_id'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

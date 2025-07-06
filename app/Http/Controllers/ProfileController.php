4261ed9eea25f0e4ca129c63bbe3a190147f3195
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 15:40:03 +0530

    commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -28,18 +28,20 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $data = $request->validated();
-        $image = $data['image'] ?? null;
+        // $image = $data['image'] ?? null;
 
-        if ($image)
-            $data['image'] = $image->store('avatars', 'public');
+        // if ($image)
+        //     $data['image'] = $image->store('avatars', 'public');
 
-        $request->user()->fill($data);
+        $user = $request->user();
+        $user->fill($data);
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        if ($user->isDirty('email')) {
+            $user->email_verified_at = null;
         }
 
-        $request->user()->save();
+        $user->save();
+        $user->addMediaFromRequest('image')->toMediaCollection();
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }

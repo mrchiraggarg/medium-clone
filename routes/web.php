ac2a49d1ccbcac8352681c1c93dfff5546eb0da3
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 13:33:26 +0530

    commit

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/@{user:username}', [PublicProfileController::class, 'show'])->name(
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', [PostController::class, 'index'])->name('dashboard');
+    Route::get('/category/{category}', [PostController::class, 'category'])->name('category.index');
     Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
     Route::post('/post/store', [PostController::class, 'store'])->name('post.store');
     Route::get('/@{username}/{post:slug}', [PostController::class, 'show'])->name('post.show');

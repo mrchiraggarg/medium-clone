fa60c5d4108633af35b39a1407a69ae206ec6e81
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 15:44:52 +0530

    commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -41,7 +41,7 @@ class ProfileController extends Controller
         }
 
         $user->save();
-        $user->addMediaFromRequest('image')->toMediaCollection();
+        $user->addMediaFromRequest('image')->toMediaCollection('avatar');
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }

beb0901d356b0e7c06edc11e9c28bd92849836d4
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 12:20:18 +0530

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClapController;
 use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
@@ -14,6 +15,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/post/store', [PostController::class, 'store'])->name('post.store');
     Route::get('/@{username}/{post:slug}', [PostController::class, 'show'])->name('post.show');
     Route::post('/follow/{user}', [FollowerController::class, 'followUnfollow'])->name('follow');
+    Route::post('/clap/{post}', [ClapController::class, 'clap'])->name('clap');
 });
 
 Route::middleware('auth')->group(function () {

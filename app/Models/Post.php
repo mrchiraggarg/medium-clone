de8bab2aa45663f4f086012832476aa75eabe32a
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 11:37:35 +0530

    commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,6 +26,11 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function clap()
+    {
+        return $this->hasMany(Clap::class);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);

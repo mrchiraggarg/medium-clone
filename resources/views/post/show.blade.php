fde3ec01ac5a20e5169bf2274bd131945e103d33
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 12:31:16 +0530

    commit

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -35,7 +35,7 @@
                     </span>
                 </div>
 
-                <x-clap-button />
+                <x-clap-button :post="$post" />
             </div>
         </div>
     </div>

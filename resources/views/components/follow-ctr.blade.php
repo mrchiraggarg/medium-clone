c9c6c3b4cce9d9f99110f256d7df5e46866076a9
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 13:03:58 +0530

    commit

--- resources/views/components/follow-ctr.blade.php
+++ resources/views/components/follow-ctr.blade.php
@@ -4,9 +4,9 @@
     following: {{ $user->isFollowedBy(auth()->user()) ? 'true' : 'false' }},
     followersCount: {{ $user->followers()->count() }},
     follow() {
-        this.following = !this.following
         axios.post('/follow/{{ $user->id }}')
             .then(res => {
+                this.following = !this.following
                 this.followersCount = res.data.followersCount
             })
             .catch(err => {

b9c8a8a3a802b635a7f34d8116222c9d17435d80
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 11:44:44 +0530

    commit

--- database/migrations/2025_07_06_054645_create_claps_table.php
+++ database/migrations/2025_07_06_054645_create_claps_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamp('created_at')->nullable();
-            $table->timestamps();
+            // $table->timestamps();
         });
     }
 

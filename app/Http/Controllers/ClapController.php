c8f7467194cc5b14fc0d54b7f3717f9399c070db
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 12:17:12 +0530

    commit

--- app/Http/Controllers/ClapController.php
+++ app/Http/Controllers/ClapController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class ClapController extends Controller
 {
-    //
+    public function clap(Post $post)
+    {
+        $post->claps()->create([
+            'user_id' => auth()->id(),
+        ]);
+
+        return back();
+    }
 }

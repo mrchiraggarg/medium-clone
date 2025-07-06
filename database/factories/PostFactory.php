983411f2954d49ff3e94b0683cb0ebfdef4d6793
mrchiraggarg <gargchirag728@gmail.com>
2025-07-06 16:00:20 +0530

    commit

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,9 +17,12 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        // $image = fake()->imageUrl();
+        $image = "https://images.unsplash.com/photo-1749741355867-8d40976f2bfb?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
         $title = fake()->sentence();
+
         return [
-            'image' => fake()->imageUrl(),
+            'image' => $image,
             'title' => $title,
             'slug' => \Illuminate\Support\Str::slug($title),
             'content' => fake()->paragraph(5),

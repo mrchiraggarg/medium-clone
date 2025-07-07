850a3baa21a12fafc89edd50ccd7007bf066e86c
mrchiraggarg <gargchirag728@gmail.com>
2025-07-07 18:01:37 +0530

    commit

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -15,7 +15,11 @@
 
         {{-- px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 --}}
 
-        <a href="javascript:;" class="text-sm text-gray-400 flex gap-4">
+        <a href="{{ route('post.show', [
+            'username' => $post->user->username,
+            'post' => $post->slug,
+        ]) }}"
+            class="text-sm text-gray-400 flex gap-4">
             {{ $post->created_at->format('M d, Y') }}
 
             <span class="inline-flex gap-1 items-center">

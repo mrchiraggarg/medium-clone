e966115a7eda1522658a9854edabcfd97ea7e97c
mrchiraggarg <gargchirag728@gmail.com>
2025-07-07 17:52:31 +0530

    commit

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -18,7 +18,7 @@
         <a href="javascript:;" class="text-sm text-gray-400 flex gap-4">
             {{ $post->created_at->format('M d, Y') }}
 
-            <span>
+            <span class="inline-flex gap-1 items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round"

57bb73a62c269fb8f2fda15c84c62d4a3a79339e
mrchiraggarg <gargchirag728@gmail.com>
2025-07-07 18:00:13 +0530

    commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,7 +22,7 @@ class PostController extends Controller
         // });
 
         $user = auth()->user();
-        $query = Post::with('user', 'media')->withCount('claps')->latest();
+        $query = Post::with(['user', 'media'])->withCount('claps')->latest();
 
         if ($user) {
             $ids = $user->following()->pluck('users.id');
@@ -104,7 +104,7 @@ class PostController extends Controller
 
     public function category(Category $category)
     {
-        $posts = $category->posts()->latest()->simplePaginate(5);
+        $posts = $category->posts()->with(['user', 'media'])->withCount('claps')->latest()->simplePaginate(5);
         return view('post.index', [
             'posts' => $posts,
         ]);

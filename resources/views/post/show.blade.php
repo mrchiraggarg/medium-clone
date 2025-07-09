afa5658137d8875a3c20512f126025fa868938eb
mrchiraggarg <gargchirag728@gmail.com>
2025-07-09 14:49:49 +0530

    commit

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -25,16 +25,18 @@
                     </div>
                 </div>
 
-                <div class="py-4 mt-8 border-t border-b border-gray-200">
-                    <x-primary-button>Edit Post</x-primary-button>
-                    <form class="inline-block" action="{{ route('post.destroy', $post) }}" method="POST">
-                        @csrf
-                        @method('delete')
+                @if ($post->user_id === Auth::id())
+                    <div class="py-4 mt-8 border-t border-b border-gray-200">
+                        <x-primary-button>Edit Post</x-primary-button>
+                        <form class="inline-block" action="{{ route('post.destroy', $post) }}" method="POST">
+                            @csrf
+                            @method('delete')
 
 
-                        <x-danger-button>Delete Post</x-danger-button>
-                    </form>
-                </div>
+                            <x-danger-button>Delete Post</x-danger-button>
+                        </form>
+                    </div>
+                @endif
 
                 <x-clap-button :post="$post" />
 

a0f362799523ca9f73d945168e9c930994aecc21
mrchiraggarg <gargchirag728@gmail.com>
2025-07-09 15:17:51 +0530

    commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,11 +10,13 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Image\Enums\Fit;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\Sluggable\HasSlug;
 
 class Post extends Model implements HasMedia
 {
     use HasFactory;
     use InteractsWithMedia;
+    use HasSlug;
 
     protected $fillable = [
         // 'image',
